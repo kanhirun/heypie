c281cb1a0d17e7e33966176efa9712c9f9917c82
Kel Kanhirun <kel.kanhirun@gmail.com>
2018-09-04 20:40:13 -0400

    Updates seed with slices of pie

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
-puts Grunt.find_or_create_by!(name: "U1UESB3TP") # daiv
-puts Grunt.find_or_create_by!(name: "U1UGEND33") # kel
-puts Grunt.find_or_create_by!(name: "UCBMB4W3C") # justin
-puts Grunt.find_or_create_by!(name: "UCD1YB1E2") # chip
-puts Grunt.find_or_create_by!(name: "UCBRKETRT") # vu
+daiv = Grunt.find_or_create_by!(name: "U1UESB3TP") # daiv
+kel = Grunt.find_or_create_by!(name: "U1UGEND33") # kel
+justin = Grunt.find_or_create_by!(name: "UCBMB4W3C") # justin
+chip = Grunt.find_or_create_by!(name: "UCD1YB1E2") # chip
+vu = Grunt.find_or_create_by!(name: "UCBRKETRT") # vu
+
+daiv.slices_of_pie = 400
+daiv.save!
+
+kel.slices_of_pie = 800
+kel.save!
+
+justin.slices_of_pie = 350
+justin.save!
+
+vu.slices_of_pie = 275
+vu.save!
+
+chip.slices_of_pie = 150
+chip.save!
